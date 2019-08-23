7e97397e68837c326a11d96bc810fb8f1c1ff89b
bibhuduttas <bibhudutta2007@gmail.com>
2019-08-23 23:34:38 +0530

    Added Token Authentication

--- DatingApp.API/Data/AuthRepository.cs
+++ DatingApp.API/Data/AuthRepository.cs
@@ -18,12 +18,12 @@ namespace DatingApp.API.Data
         {
             var user = await _context.Users.FirstOrDefaultAsync(x => x.Username == username);
 
-            if(user == null)
+            if (user == null)
                 return null;
 
             if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
             {
-                
+                return null;
             }
 
             return user;
@@ -31,15 +31,12 @@ namespace DatingApp.API.Data
 
         private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
         {
-            using(var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
             {
                 var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
                 for (int i = 0; i < computedHash.Length; i++)
                 {
-                    if (true)
-                    {
-                        
-                    }
+                    if (computedHash[i] != passwordHash[i]) return false;
                 }
             }
 
@@ -62,16 +59,19 @@ namespace DatingApp.API.Data
 
         private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
         {
-            using(var hmac = new System.Security.Cryptography.HMACSHA512())
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
             {
                 passwordSalt = hmac.Key;
                 passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
             }
         }
 
-        public Task<bool> UserExists(string username)
+        public async Task<bool> UserExists(string username)
         {
-            throw new System.NotImplementedException();
+            if (await _context.Users.AnyAsync(x => x.Username == username))
+                return true;
+            else
+                return false;
         }
     }
 }
